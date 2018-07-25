3f4105fd5fe1e0b2684f34de98dbc68f94635bef
Eloa Paradela <eloparadela@gmail.com>
2018-07-24 21:13:53 -0300

    update com sucesso

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
- let nome = document.querySelector('#idname');
- let entradas = document.querySelectorAll('input');
+let nome = document.querySelector('#idname');
+ //let entradas = document.querySelectorAll('input');
  let sobrenome = document.querySelector('#idsobrenome');
  let email = document.querySelector('#idemail');
  let botao = document.querySelector("#botao");
- let paragrafo = document.querySelectorAll('p');
+ // let paragrafo = document.querySelectorAll('p');
 
 
 botao.addEventListener('click', enviar); 
 
 
-function enviar(){
-    if(confirm ("deseja confirmar?")){
-    alert('Obrigado(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-     }   
-    if(nome == "" || nome == null ){
+function enviar(){ 
+    if(nome.value == "" || nome.value == null ){
      alert('Favor preencher o nome');
        }
-    if(sobrenome == "" || nome == null ){
+    else if(sobrenome.value == "" || sobrenome.value == null ){
         alert('Favor preencher o sobrenome');
        }
-    if(email == "" || email == null ){
+    else if (email.value == "" || email.value == null ){
         alert('Favor preencher o email');
+       }
+    else{
+        alert('Obrigado(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
        }
     }
